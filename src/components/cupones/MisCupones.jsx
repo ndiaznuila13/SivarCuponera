cb3504e8a50d84df2e395965addbe5a552b35999
Nicky2811 <carcamojade@gmail.com>
2026-02-20 17:31:29 -0600

    sjds

---
+++ src/components/cupones/MisCupones.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MisCupones = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Mis Cupones</h2>
+      <p className="text-gray-600">Aquí puedes ver tus cupones disponibles.</p>
+    </div>
+
+    
+  )
+}
+
+export default MisCupones
