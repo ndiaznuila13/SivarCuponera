b3d9220ab82e5472dcd3e7464477131f7df99244
Nicky2811 <carcamojade@gmail.com>
2026-02-21 03:59:24 -0600

    cambio por ruta

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRoutes from './routes/Route';
-import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
       <AppRoutes />
-    </AuthProvider>
   </StrictMode>,
-)
+);
