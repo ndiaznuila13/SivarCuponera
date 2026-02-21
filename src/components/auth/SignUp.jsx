25ab89d8db44a6c4b0b55c087ba5bd6f2f5d0c3c
AlxMC2 <20245146@esen.edu.sv>
2026-02-20 21:47:26 -0600

    Vista de la página registro

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function SignUp() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-[var(--color-background-light)] px-4">
 
@@ -39,7 +43,8 @@ export default function SignUp() {
                                 </label>
                                 <input
                                     type="text"
-                                    placeholder="Alexander"
+                                    placeholder="Juan"
+                                    required
                                     className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
                                 />
                             </div>
@@ -50,7 +55,8 @@ export default function SignUp() {
                                 </label>
                                 <input
                                     type="text"
-                                    placeholder="Martínez"
+                                    placeholder="Pérez"
+                                    required
                                     className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
                                 />
                             </div>
@@ -65,6 +71,7 @@ export default function SignUp() {
                             <input
                                 type="email"
                                 placeholder="correo@email.com"
+                                required
                                 className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
                             />
                         </div>
@@ -74,11 +81,40 @@ export default function SignUp() {
                             <label className="block text-sm font-medium text-gray-700 mb-1">
                                 Contraseña
                             </label>
-                            <input
-                                type="password"
-                                placeholder="••••••••"
-                                className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
-                            />
+
+                            <div className="relative">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    required
+                                    className="w-full rounded-lg border border-gray-300 px-4 py-2 pr-12 focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
+                                />
+
+                                {/* Botón ojito */}
+                                <button
+                                    type="button"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    className="absolute inset-y-0 right-0 flex items-center pr-4 text-gray-500 hover:text-gray-700 hover:cursor-pointer"
+                                >
+                                    {showPassword ? (
+                                        // Ojo abierto
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7
+                      -1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                        </svg>
+                                    ) : (
+                                        // Ojo cerrado
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                                d="M13.875 18.825A10.05 10.05 0 0112 19
+                      c-4.478 0-8.268-2.943-9.542-7a9.956 9.956 0 012.223-3.592M6.18 6.18A9.953 9.953 0 0112 5
+                      c4.478 0 8.268 2.943 9.542 7a9.96 9.96 0 01-4.293 5.067M15 12a3 3 0 00-4.243-2.828M3 3l18 18" />
+                                        </svg>
+                                    )}
+                                </button>
+                            </div>
                         </div>
 
                         {/* DUI y Teléfono */}
@@ -91,6 +127,8 @@ export default function SignUp() {
                                 <input
                                     type="text"
                                     placeholder="00000000-0"
+                                    pattern="[0-9]{8}-[0-9]{1}"
+                                    required
                                     className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
                                 />
                             </div>
@@ -101,7 +139,9 @@ export default function SignUp() {
                                 </label>
                                 <input
                                     type="tel"
-                                    placeholder="7000-0000"
+                                    placeholder="0000-0000"
+                                    pattern="[0-9]{4}-[0-9]{4}"
+                                    required
                                     className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[var(--color-primary)]"
                                 />
                             </div>
